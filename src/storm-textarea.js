c31b274a3cc99b0ff39d0e91df5862ee39bf1d9b
Federico Brigante <github@bfred.it>
2018-03-25 01:45:34 +0800

    Modernize/reduce

--- src/storm-textarea.js
+++ src/storm-textarea.js
@@ -1,37 +1,29 @@
-
 const defaults = {
-		paddingBottom: 16,
-		events: [
-			'focus',
-			'cut',
-			'drop',
-			'keydown',
-			'resize',
-			'change'
-		]
-	},
-	StormTextarea = {
-		init() { 
-			this.settings.events.forEach(evt => this.DOMElement.addEventListener(evt, this.resize.bind(this)));
-			return this;
-		},
-		resize() {
-			this.DOMElement.style.height = 'auto';
-			this.DOMElement.style.height =  this.DOMElement.scrollHeight + this.settings.paddingBottom + 'px';
-		}
-	};
-	
-const init = (sel, opts) => {
-	let els = [].slice.call(document.querySelectorAll(sel));
-	
-	if(els.length === 0) throw new Error('Textarea cannot be initialised, no augmentable elements found');
-	
-	return els.map(el => {
-		return Object.assign(Object.create(StormTextarea), {
-			DOMElement: el,
-			settings: Object.assign({}, defaults, opts)
-		}).init();
-	});
+	events: [
+		'input'
+	]
 };
 
-export default { init };
+function resize({target}) {
+	target.style.height = 'auto';
+	target.style.height =  target.scrollHeight + 'px';
+}
+
+function listen (event) {
+	this.addEventListener(event, resize);
+}
+
+export default function (elements, settings) {
+	if (typeof elements === 'string') {
+		elements = document.querySelectorAll(elements);
+	}
+
+	if(elements.length === 0) {
+		return;
+	}
+
+	const currentSettings = Object.assign({}, defaults, settings);	
+	elements.forEach(element => {
+		currentSettings.events.forEach(listen, element);
+	});
+}
