ac8e7e12e5851ff21d10cbe03db7ab70f9ffb15a
Federico Brigante <github@bfred.it>
2018-03-25 01:58:37 +0800

    Drop .forEach for speed (or maybe not)

--- src/storm-textarea.js
+++ src/storm-textarea.js
@@ -9,10 +9,6 @@ function update({target}) {
 	target.style.height =  target.scrollHeight + 'px';
 }
 
-function listen (event) {
-	this.addEventListener(event, update);
-}
-
 export default function (elements, settings) {
 	if (typeof elements === 'string') {
 		elements = document.querySelectorAll(elements);
@@ -22,9 +18,11 @@ export default function (elements, settings) {
 		return;
 	}
 
-	const events = settings && settings.events || defaults.events;	
-	elements.forEach(element => {
-		currentSettings.events.forEach(listen, element);
+	const events = settings && settings.events || defaults.events;
+	for (let i = 0; i < elements.length; i++) {
+		for (let l = 0; l < events.length; i++) {
+			elements[i].addEventListener(events[l], update);
+		}
 		update({target: element});
-	});
+	}
 }
